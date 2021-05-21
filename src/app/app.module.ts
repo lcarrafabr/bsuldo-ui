c82ac0e9e790cb6c7f6064ccc14a361ba214c7d9
lcarrafabr <lcarrafa.br@gmail.com>
2021-05-21 17:06:03 -0300

    Criando modulo de lançamentos e ajustando as arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,10 +9,8 @@ import {CalendarModule} from 'primeng/calendar';
 import {DropdownModule} from 'primeng/dropdown';
 import { TableModule } from 'primeng/table';
 import {TooltipModule} from 'primeng/tooltip';
-import { LancamentoPesquisaComponent } from './lancamentos/lancamento-pesquisa/lancamento-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import {CardModule} from 'primeng/card';
-import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
 import {InputSwitchModule} from 'primeng/inputswitch';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {InputNumberModule} from 'primeng/inputnumber';
@@ -19,30 +18,21 @@ import { FormsModule } from '@angular/forms';
 import {MessagesModule} from 'primeng/messages';
 import {MessageModule} from 'primeng/message';
 import { MessageComponent } from './message/message.component';
+import {SidebarModule} from 'primeng/sidebar';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LancamentoPesquisaComponent,
     NavbarComponent,
-    LancamentoCadastroComponent,
     MessageComponent
   ],
   imports: [
     BrowserModule,
-    ButtonModule,
-    InputTextModule,
-    CalendarModule,
-    DropdownModule,
-    TableModule,
-    TooltipModule,
-    CardModule,
-    InputSwitchModule,
     BrowserAnimationsModule,
-    InputNumberModule,
-    FormsModule,
-    MessagesModule,
-    MessageModule
+    SidebarModule,
+
+    LancamentosModule
 
 
   ],
---
+++ src/app/lancamentos/lancamentos.module.ts
@@ -0,0 +1,50 @@
+import { SidebarModule } from 'primeng/sidebar';
+import { MessagesModule } from 'primeng/messages';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { CardModule } from 'primeng/card';
+import { TooltipModule } from 'primeng/tooltip';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { LancamentoPesquisaComponent } from './lancamento-pesquisa/lancamento-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MessageModule } from 'primeng/message';
+
+
+
+@NgModule({
+  declarations: [
+    LancamentoCadastroComponent,
+    LancamentoPesquisaComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    CalendarModule,
+    DropdownModule,
+    TableModule,
+    TooltipModule,
+    CardModule,
+    InputSwitchModule,
+    BrowserAnimationsModule,
+    InputNumberModule,
+    FormsModule,
+    MessagesModule,
+    MessageModule,
+    SidebarModule,
+  ],
+  exports: [
+    LancamentoCadastroComponent,
+    LancamentoPesquisaComponent
+  ]
+})
+export class LancamentosModule { }
